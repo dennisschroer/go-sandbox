e9b4328c300288db36d14a186480ef063fc8493f
dennis.schroer <dennis.schroer@sqills.com>
2022-02-10 11:40:29 +0100

    ♻️ Split into methods

--- hello.go
+++ hello.go
@@ -11,59 +11,89 @@ import (
 
 const maxTurns = 6
 
-func main() {
-	fmt.Println("Welcome to Wordle!")
+type WordleState struct {
+	word string
+	turn int
+	done bool
+}
 
-	// Read words
-	data, err := os.ReadFile("words")
+func main() {
+	playGame()
+}
+
+func playGame() {
+	words, err := loadWords()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	words := strings.Split(string(data), "\n")
 
-	// Select a random word
-	rand.Seed(time.Now().UnixNano())
-	randomIndex := rand.Intn(len(words))
-	word := words[randomIndex]
-	//fmt.Printf("Debug; The random index is %d, the word is: %s\n", randomIndex, word)
+	fmt.Println("Welcome to Wordle!")
+	state := &WordleState{word: randomWord(words)}
 
-	// Do turns
-	turn := 0
-	var input string
-
-	for turn < maxTurns {
-		fmt.Printf("Turn %d, guess: ", turn)
-		_, err := fmt.Scanln(&input)
-
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		if len(input) != 5 {
-			fmt.Println("Invalid, word should be 5 characters long")
-			continue
-		}
-
-		if input == word {
-			color.Green("You win! The word was: %s", word)
-			return
-		}
-
-		for index, char := range input {
-			if uint8(char) == word[index] {
-				fmt.Print(color.HiGreenString("%c", char))
-			} else if strings.ContainsRune(word, char) {
-				fmt.Print(color.HiYellowString("%c", char))
-			} else {
-				fmt.Print(color.WhiteString("%c", char))
-			}
-		}
-		fmt.Println()
-
-		turn++
+	for state.turn < maxTurns && !state.done {
+		doTurn(state)
 	}
 
-	color.Red("You lose! The word was: %s", word)
+	if !state.done {
+		color.Red("You lose! The word was: %s", state.word)
+	}
+}
+
+func loadWords() ([]string, error) {
+	data, err := os.ReadFile("words")
+	if err != nil {
+		return nil, err
+	}
+	return strings.Split(string(data), "\n"), nil
+}
+
+func randomWord(words []string) string {
+	rand.Seed(time.Now().UnixNano())
+	return words[rand.Intn(len(words))]
+}
+
+func doTurn(state *WordleState) {
+	input, err := requestInput(state)
+	if err != nil {
+		fmt.Println(err)
+		state.done = true
+		return
+	}
+
+	if len(input) != 5 {
+		fmt.Println("Invalid, word should be 5 characters long")
+		return
+	}
+
+	if input == state.word {
+		color.Green("You win! The word was: %s", state.word)
+		state.done = true
+		return
+	}
+
+	for index, char := range input {
+		if uint8(char) == state.word[index] {
+			fmt.Print(color.HiGreenString("%c", char))
+		} else if strings.ContainsRune(state.word, char) {
+			fmt.Print(color.HiYellowString("%c", char))
+		} else {
+			fmt.Print(color.WhiteString("%c", char))
+		}
+	}
+	fmt.Println()
+
+	state.turn++
+}
+
+func requestInput(state *WordleState) (string, error) {
+	fmt.Printf("Turn %d, guess: ", state.turn)
+
+	var input string
+	_, err := fmt.Scanln(&input)
+	if err != nil {
+		return "", err
+	}
+
+	return input, nil
 }
